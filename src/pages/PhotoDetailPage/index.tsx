18fa719e85a4f01c3ce077f3142c8710ad96e75e
Marius Pettersen <marius.pettersen86@gmail.com>
2023-11-27 13:48:54 +0100

    Added photoDetailPage

---
+++ src/pages/PhotoDetailPage/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PhotoDetailPage = () => {
+  return (
+    <View>
+      <Text>PhotoDetailPage</Text>
+    </View>
+  )
+}
+
+export default PhotoDetailPage
