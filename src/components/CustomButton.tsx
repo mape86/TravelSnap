0eaa1f0cab0aef332f365e559f54e075c6a85de6
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2023-12-05 10:54:50 +0100

    Added functionality for signing up as a user. (#4)

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,71 +1,70 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import * as React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
 
 type EntypoIconName =
-| "key"
-| "link"
-| "email"
-| "image"
-| "text"
-| "menu"
-| "radio"
-| "switch"
-| "address"
-| "code"
-| "map"
-| "video"
-| "circle"
-| "mask"
-| "forward"
-| "back"
-| "retweet"
-| "minus"
-| "plus"
-| "camera"
-| "check"
-| "flash"
-| "icloud"
-| "cloud"
-| "upload";
-
-
+  | "key"
+  | "link"
+  | "email"
+  | "image"
+  | "text"
+  | "menu"
+  | "radio"
+  | "switch"
+  | "address"
+  | "code"
+  | "map"
+  | "video"
+  | "circle"
+  | "mask"
+  | "forward"
+  | "back"
+  | "retweet"
+  | "minus"
+  | "plus"
+  | "camera"
+  | "check"
+  | "flash"
+  | "icloud"
+  | "cloud"
+  | "upload";
 
 interface ButtonProps {
-    onPress: () => void;
-    title: string;
-    iconName?: EntypoIconName;
-    color?: string;
+  onPress: () => void;
+  title: string;
+  iconName?: EntypoIconName;
+  color?: string;
+  disabled?: boolean;
 }
 
-const CustomButton = ({ onPress, title, iconName, color }: ButtonProps) => (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-        {iconName && (
-            <Entypo
-                name={iconName}
-                size={24}
-                color={color || "white"}
-            />
-        )}
-        <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
+const CustomButton = ({
+  onPress,
+  title,
+  iconName,
+  color,
+  disabled,
+}: ButtonProps) => (
+  <TouchableOpacity onPress={onPress} style={styles.button} disabled={disabled}>
+    {iconName && <Entypo name={iconName} size={24} color={color || "white"} />}
+    <Text style={styles.text}>{title}</Text>
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    button: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: "#000",
-    },
-    text: {
-        marginLeft: 8,
-        color: "white",
-    },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#000",
+  },
+  text: {
+    marginLeft: 8,
+    color: "white",
+  },
 });
 
 export default CustomButton;
