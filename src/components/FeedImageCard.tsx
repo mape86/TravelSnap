f94d83be5d8de7a38bffd41451da23650c3e8ad0
Marianne <91476569+marianneindreb@users.noreply.github.com>
2023-12-05 22:59:28 +0100

    added navigation from homepage to detailpage

--- src/components/FeedImageCard.tsx
+++ src/components/FeedImageCard.tsx
@@ -3,23 +3,30 @@ import React, { useState } from "react";
 import { View, Image, Text, TouchableOpacity } from "react-native";
 import { flingGestureHandlerProps } from "react-native-gesture-handler/lib/typescript/handlers/FlingGestureHandler";
 import Assets from "../Assets";
+import useCustomNavigation from "../hooks/Navigation/useCustomNavigation";
 import { FeedImage, feedImages } from "./constants";
 
 interface FeedImageCardProps {
   location: string;
   feedImage: any;
-  iconImage: any;
+  id: string;
 }
 
 const FeedImageCard: React.FC<FeedImageCardProps> = ({
   location,
   feedImage,
-  iconImage,
+  id,
 }) => {
+  const { navigate } = useCustomNavigation();
   return (
     <View>
       <TouchableOpacity
-        /*onPress={}*/
+        onPress={() =>
+          navigate("PhotoDetailPage", {
+            id,
+            feedImage,
+          })
+        }
         className="flex items-center relative  mb-5"
       >
         <Image
@@ -28,7 +35,8 @@ const FeedImageCard: React.FC<FeedImageCardProps> = ({
           className=" aspect-ratio-ratio-1 rounded-lg"
         />
         <View className="absolute bottom-2 right-10 items-center justify-center w-12 h-12 bg-white rounded-full overflow-hidden">
-          <Image source={iconImage} className="w-5 h-5 " />
+          {/* TODO: Add heart icon */}
+          {/* <Image source='' className="w-5 h-5 " /> */}
         </View>
         <Text className="font-semibold mt-2">{location}</Text>
       </TouchableOpacity>
--- src/pages/PhotoDetailPage/index.tsx
+++ src/pages/PhotoDetailPage/index.tsx
@@ -1,28 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
+
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import useCustomNavigation from '../../hooks/Navigation/useCustomNavigation'
-import { RouteList } from '../../hooks/Navigation/useCustomNavigation'
-import Navigator from '../../components/Navigator';
+import useCustomNavigation from "../../hooks/Navigation/useCustomNavigation";
+import { RouteList } from "../../hooks/Navigation/useCustomNavigation";
+import Navigator from "../../components/Navigator";
+import Assets from "../../Assets";
 
-
-const PhotoDetailPage = () => {
-
-  const { navigate } = useCustomNavigation();
-
-  const handleClick = (item: keyof RouteList) => {
-    navigate(item);
-  };
-
-  return (
-    <SafeAreaView className='flex-1 justify-center items-center'>
-      <TouchableOpacity onPress={() => handleClick("UserProfilePage")}>
-        <Text>Photo owner user page</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
+interface PhotoDetailPageProps {
+  location: string;
+  feedImage: any;
+  id: string;
 }
 
-export default PhotoDetailPage
+const PhotoDetailPage = (props: any) => {
+  const { navigate } = useCustomNavigation();
+
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <ScrollView className="bg-white">
+        <Image source={props.route.params.feedImage} className="w-full h-80" />
+
+        <TouchableOpacity onPress={() => navigate("ProfilePage")}>
+          <View className="flex-row items-center">
+            <Image
+              className="rounded-lg m-5"
+              style={{ width: 60, height: 75 }}
+              source={Assets.images.ProfileImage}
+            />
+            <Text className="ml-2 text-lg font-bold">@susanna.winther</Text>
+          </View>
+        </TouchableOpacity>
+        <View className="p-4">
+          <Text className="text-xl font-bold">Location</Text>
+          <Text className="text-base text-gray-800">
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet.
+          </Text>
+          <Text className="mt-1 text-blue-500">#Dolor #Sit #Amet</Text>
+        </View>
+
+        {/* Use react-native-maps */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default PhotoDetailPage;
