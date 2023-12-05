92e5d61c37722680e960e2f628d19a0e7a6fd677
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2023-12-05 12:10:31 +0100

    Added bg-color to button, login functionality.

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import useCustomNavigation  from '../../hooks/Navigation/useCustomNavigation';
-import { RouteList } from '../../hooks/Navigation/useCustomNavigation'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View } from "react-native";
+import React, { useState } from "react";
+import useCustomNavigation from "../../hooks/Navigation/useCustomNavigation";
+import { TextInput } from "react-native-paper";
+import CustomButton from "../../components/CustomButton";
+import { fbAuth } from "../../../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginPage = () => {
-
   const { navigate } = useCustomNavigation();
 
   const { goBack } = useCustomNavigation();
@@ -14,14 +15,54 @@ const LoginPage = () => {
     goBack();
   };
 
-  return (
-    <View className='flex-1 justify-center items-center'>
-      <Text>LoginPage</Text>
-      <TouchableOpacity onPress={handleReturnClick}>
-        <Text>Return</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+  const auth = fbAuth;
 
-export default LoginPage
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const userLogin = async () => {
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert("User logged in successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View className="mt-52">
+      <View className="px-10">
+        <TextInput
+          label="Email"
+          placeholder="email"
+          value={email}
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          className="mb-10"
+          label="Password"
+          placeholder="password"
+          autoCapitalize="none"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+        />
+
+        <View className="mb-5">
+          <CustomButton onPress={userLogin} title="Sign In" />
+        </View>
+        <View>
+          <CustomButton
+            onPress={handleReturnClick}
+            title="Go back"
+            backgroundColor="bg-neutral-400"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default LoginPage;
