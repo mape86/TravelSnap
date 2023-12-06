50cf684e46c36b6af7479eb5f1c501ca12954dbb
Marianne <91476569+marianneindreb@users.noreply.github.com>
2023-12-06 16:44:31 +0100

    Photo detail page (#11)

--- src/hooks/Navigation/useCustomNavigation.tsx
+++ src/hooks/Navigation/useCustomNavigation.tsx
@@ -1,43 +1,47 @@
-import { useNavigation, NavigationProp } from "@react-navigation/native";
+import {
+  useNavigation,
+  NavigationProp,
+  ParamListBase,
+  NavigatorScreenParams,
+} from "@react-navigation/native";
 
 export type RouteList = {
+  //Welcome routes:
+  Welcome: undefined;
+  LoginPage: undefined;
+  CreateUserPage: undefined;
 
-    //Welcome routes:
-    Welcome: undefined;
-    LoginPage: undefined;
-    CreateUserPage: undefined;
+  //Shared routes:
+  ProfilePage: undefined;
+  PhotoDetailPage: undefined;
 
-    //Shared routes:
-    ProfilePage: undefined;
-    PhotoDetailPage: undefined;
+  //Homepage routes:
+  HomeRoutes: undefined;
+  HomePage: undefined;
 
-    //Homepage routes:
-    HomeRoutes: undefined;
-    HomePage: undefined;
+  //Photo/Camera pages:
+  CameraPage: undefined;
+  PhotoGalleryPage: undefined;
+  //Search pages:
 
-    //Photo/Camera pages:
-    CameraPage: undefined;
-    PhotoGalleryPage: undefined;
-    //Search pages:
-    
-    //UserProfile pages:
-    UserProfilePage: undefined;
-    UserSettingsPage: undefined;
-}
+  //UserProfile pages:
+  UserProfilePage: undefined;
+  UserSettingsPage: undefined;
+};
 
 type TravelSnapNavigationProp = NavigationProp<RouteList>;
 
 const useCustomNavigation = () => {
-    const navigation = useNavigation<TravelSnapNavigationProp>();
-    
-    const navigate = (path: keyof RouteList) => {
-        navigation.navigate(path);
-    }
-    const goBack = () => {
-        navigation.goBack();
-    }
+  const navigation = useNavigation<TravelSnapNavigationProp>();
 
-    return { navigate, goBack };
-}
+  const navigate = (path: keyof RouteList, params?: any) => {
+    navigation.navigate(path, params);
+  };
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  return { navigate, goBack };
+};
 
 export default useCustomNavigation;
--- src/pages/PhotoDetailPage/index.tsx
+++ src/pages/PhotoDetailPage/index.tsx
@@ -1,28 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
+
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import useCustomNavigation from '../../hooks/Navigation/useCustomNavigation'
-import { RouteList } from '../../hooks/Navigation/useCustomNavigation'
-import Navigator from '../../components/Navigator';
+import useCustomNavigation from "../../hooks/Navigation/useCustomNavigation";
+import { RouteList } from "../../hooks/Navigation/useCustomNavigation";
+import Navigator from "../../components/Navigator";
+import Assets from "../../Assets";
 
-
-const PhotoDetailPage = () => {
-
-  const { navigate } = useCustomNavigation();
-
-  const handleClick = (item: keyof RouteList) => {
-    navigate(item);
-  };
-
-  return (
-    <SafeAreaView className='flex-1 justify-center items-center'>
-      <TouchableOpacity onPress={() => handleClick("UserProfilePage")}>
-        <Text>Photo owner user page</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
+interface PhotoDetailPageProps {
+  location: string;
+  feedImage: any;
+  id: string;
 }
 
-export default PhotoDetailPage
+const PhotoDetailPage = (props: any) => {
+  const { navigate } = useCustomNavigation();
+
+  return (
+    <ScrollView className="bg-brandLight ">
+      <Image source={props.route.params.feedImage} className="w-full h-96" />
+      <SafeAreaView className="flex-1">
+        <TouchableOpacity onPress={() => navigate("ProfilePage")}>
+          <View className="flex-row items-center">
+            <Image
+              className="rounded-lg m-3"
+              style={{ width: 60, height: 75 }}
+              source={Assets.images.ProfileImage}
+            />
+            <Text className=" text-lg font-bold">@susanna.winther</Text>
+          </View>
+        </TouchableOpacity>
+        <View className="p-4">
+          <Text className="text-xl font-bold">Location</Text>
+          <Text className="text-base text-gray-800">
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet.
+          </Text>
+          <Text className="mt-1 text-gray-500">#Dolor #Sit #Amet</Text>
+        </View>
+
+        {/* Use react-native-maps */}
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+export default PhotoDetailPage;
