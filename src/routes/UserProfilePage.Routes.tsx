76f680e5c6b5144e4a029227dca1f00b20cf90cb
Marius Pettersen <90511658+mape86@users.noreply.github.com>
2023-12-07 12:26:10 +0100

    Fixed download from Db,userProfilePage and routing (#14)

--- src/routes/UserProfilePage.Routes.tsx
+++ src/routes/UserProfilePage.Routes.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import UserProfilePage from "../pages/UserProfilePage";
 import UserSettingsPage from "../pages/UserSettingsPage";
+import PhotoDetailPage from "../pages/PhotoDetailPage";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -15,6 +16,7 @@ export const UserProfilePageRoutes = () => {
         <Navigator initialRouteName="UserProfilePage">
             <Screen name="UserProfilePage" component={UserProfilePage} options={noHeaderShown} />
             <Screen name="UserSettingsPage" component={UserSettingsPage} />
+            <Screen name="PhotoDetailPage" component={PhotoDetailPage} />
         </Navigator>
         </NavigationContainer>
     );
