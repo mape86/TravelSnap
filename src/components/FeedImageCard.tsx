b3e5630eb2aaec87feaa9e7f330599529942aa5d
Marianne <91476569+marianneindreb@users.noreply.github.com>
2023-12-09 20:31:21 +0100

    Added favorite icon to homepage

--- src/components/FeedImageCard.tsx
+++ src/components/FeedImageCard.tsx
@@ -5,6 +5,13 @@ import { flingGestureHandlerProps } from "react-native-gesture-handler/lib/types
 import Assets from "../Assets";
 import useCustomNavigation from "../hooks/Navigation/useCustomNavigation";
 import { FeedImage, feedImages } from "./constants";
+import { Ionicons } from "@expo/vector-icons";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  runOnJS,
+} from "react-native-reanimated";
 
 interface FeedImageCardProps {
   location: string;
@@ -18,6 +25,28 @@ const FeedImageCard: React.FC<FeedImageCardProps> = ({
   id,
 }) => {
   const { navigate } = useCustomNavigation();
+  const [isFavorited, setIsFavorited] = useState(false);
+  const scale = useSharedValue(1);
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+    scale.value = withSpring(isFavorited ? 1 : 1.2, {}, () => {
+      if (isFavorited) {
+        runOnJS(resetScale)();
+      }
+    });
+  };
+
+  const resetScale = () => {
+    scale.value = withSpring(1);
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
   return (
     <View>
       <TouchableOpacity
@@ -27,18 +56,25 @@ const FeedImageCard: React.FC<FeedImageCardProps> = ({
             feedImage,
           })
         }
-        className="flex items-center relative  mb-5"
+        className="flex items-center relative mb-5"
       >
         <Image
           source={feedImage}
           style={{ width: 350, height: 430 }}
           className=" aspect-ratio-ratio-1 rounded-lg"
         />
-        <View className="absolute bottom-2 right-10 items-center justify-center w-12 h-12 bg-white  rounded-full overflow-hidden">
-          {/* TODO: Add heart icon */}
-          {/* <Image source='' className="w-5 h-5 " /> */}
+        <View className="absolute bottom-0 right-10 items-center justify-center w-16 h-16 bg-system-brandLight rounded-full overflow-hidden">
+          <TouchableOpacity onPress={toggleFavorite}>
+            <Animated.View style={animatedStyle}>
+              {isFavorited ? (
+                <Ionicons name="ios-heart-sharp" size={24} color="black" />
+              ) : (
+                <Ionicons name="ios-heart-outline" size={24} color="black" />
+              )}
+            </Animated.View>
+          </TouchableOpacity>
         </View>
-        <Text className="font-semibold mt-2">{location}</Text>
+        <Text className="font-semibold mt-2 ">{location}</Text>
       </TouchableOpacity>
     </View>
   );
