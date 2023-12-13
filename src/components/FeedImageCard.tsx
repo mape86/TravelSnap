91ef3e6db74d4d69171f959740b97ae441d6c37d
Marianne <91476569+marianneindreb@users.noreply.github.com>
2023-12-13 23:30:38 +0100

    like on photodetail page

--- src/components/FeedImageCard.tsx
+++ src/components/FeedImageCard.tsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import {
-  getFirestore,
-  doc,
-  updateDoc,
-  collection,
-  query,
-  getDocs,
-  where,
-  getDoc,
-  setDoc,
-  deleteDoc,
-} from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 import { Ionicons } from "@expo/vector-icons";
-import Animated, {
-  useSharedValue,
-  withSpring,
-  useAnimatedStyle,
-  runOnJS,
-} from "react-native-reanimated";
+import { useIsFocused } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import Animated from "react-native-reanimated";
 import useCustomNavigation from "../hooks/Navigation/useCustomNavigation";
 import { ImageObject } from "../hooks/useFeedImages";
+import { useLikeImage } from "../hooks/useLike";
 
 interface FeedImageCardProps {
   image: ImageObject;
@@ -29,105 +13,18 @@ interface FeedImageCardProps {
 
 const FeedImageCard: React.FC<FeedImageCardProps> = ({ image }) => {
   const { navigate } = useCustomNavigation();
-  const [isLiked, setIsLiked] = useState(false);
-  const [likeCount, setLikeCount] = useState<number>(0);
-  const scale = useSharedValue(1);
-
-  const auth = getAuth();
-  const user = auth.currentUser;
+  const isFocused = useIsFocused();
   const imageIdPath = decodeURIComponent(image.uri).replace(
     "https://firebasestorage.googleapis.com/v0/b/travelsnap-84d7a.appspot.com/o/feed/",
     ""
   );
-
-  const fetchLikes = async () => {
-    const firestore = getFirestore();
-    const imageAttributesCollection = collection(
-      firestore,
-      "ImageAttributes",
-      imageIdPath,
-      "likes"
-    );
-
-    try {
-      const q = query(imageAttributesCollection);
-      const querySnapshot = await getDocs(q);
-
-      setLikeCount(querySnapshot.docs.length);
-
-      if (user) {
-        const userLikeDoc = await getDoc(doc(imageAttributesCollection, user.uid));
-        setIsLiked(userLikeDoc.exists());
-      }
-    } catch (error) {
-      console.error("Error fetching likes:", error);
-    }
-  };
+  const { toggleLike, isLiked, animatedStyle, fetchLikes } = useLikeImage({ imageIdPath });
 
   useEffect(() => {
-    fetchLikes();
-  }, []);
-
-  const setLike = async () => {
-    if (!user) {
-      return;
+    if (isFocused) {
+      fetchLikes();
     }
-    setIsLiked(true);
-    scale.value = withSpring(1.2, {}, () => {
-      runOnJS(resetScale)();
-    });
-
-    try {
-      const firestore = getFirestore();
-      const userId = user.uid;
-
-      const likeRef = doc(firestore, "ImageAttributes", imageIdPath, "likes", userId);
-
-      await setDoc(likeRef, { like: true });
-      console.log("Image liked successfully");
-    } catch (error) {
-      setIsLiked(false);
-      console.error("Error liking image:", error);
-    }
-  };
-
-  const unsetLike = async () => {
-    if (!user) {
-      return;
-    }
-
-    setIsLiked(false);
-    try {
-      const firestore = getFirestore();
-      const userId = user.uid;
-
-      const likeRef = doc(firestore, "ImageAttributes", imageIdPath, "likes", userId);
-
-      await deleteDoc(likeRef);
-      console.log("Image disliked successfully");
-    } catch (error) {
-      setIsLiked(true);
-      console.error("Error disliking image:", error);
-    }
-  };
-
-  const toggleLike = async () => {
-    if (isLiked) {
-      await unsetLike();
-    } else {
-      await setLike();
-    }
-  };
-
-  const resetScale = () => {
-    scale.value = withSpring(1);
-  };
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }],
-    };
-  });
+  }, [isFocused]);
 
   return (
     <View>
